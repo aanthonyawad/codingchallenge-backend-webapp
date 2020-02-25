443b108279b304f8e68fa3cebd7c1632fb30e50d
skhg <higgins@iplytics.com>
2020-02-25 16:06:18 +0100

    ctor for patent

--- src/main/java/de/iplytics/codingchallenge_backend_webapp/patents/Patent.java
+++ src/main/java/de/iplytics/codingchallenge_backend_webapp/patents/Patent.java
@@ -1,7 +1,9 @@
 package de.iplytics.codingchallenge_backend_webapp.patents;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -10,6 +12,8 @@ import java.time.LocalDate;
 @Data
 @Builder
 @Entity(name = "patent")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Patent {
 
     @Id
