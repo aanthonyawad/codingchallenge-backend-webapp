96a07a8263e3079e11e4a0b6d9dcf2b3a4a0e987
skhg <higgins@iplytics.com>
2020-02-26 10:56:55 +0100

    add a sample ISO standard

--- src/main/resources/db/migration/V1__createTables.sql
+++ src/main/resources/db/migration/V1__createTables.sql
@@ -3,3 +3,9 @@ create table patent (
     publication_date DATE NOT NULL,
     title VARCHAR(255) NOT NULL
 );
+
+create table standard (
+    standard_id VARCHAR(255) NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description LONGTEXT NOT NULL
+);
